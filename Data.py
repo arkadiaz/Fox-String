c8dc764636bfc16108229736f172cd958f3aba20
arkadiaz <kneko1411@gmail.com>
2022-02-10 14:32:32 +0700

    pertytydan string

---
+++ Data.py
@@ -0,0 +1,65 @@
+from pyrogram.types import InlineKeyboardButton
+
+
+class Data:
+    # Start Message
+    START = """
+Halo {}
+
+Selamat datang {}
+
+Jika kamu tidak percaya bot ini, 
+1) gausah baca pesan ini
+2) blokir bot atau delete chat
+
+Bot ini Bekerja Untuk Membantu Kamu Mendapatkan String Session Via Bot. Rekomendasi Jika Ingin Mengambil String Gunakan Akun Lain, Agar Tidak Delay. Terimakasih
+By @triplenineee
+    """
+
+    # Home Button
+    home_buttons = [
+        [InlineKeyboardButton("sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ sᴇssɪᴏɴ​", callback_data="generate")],
+        [InlineKeyboardButton(text="ʙᴀᴄᴋ​", callback_data="home")]
+    ]
+
+    generate_button = [
+        [InlineKeyboardButton("sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ sᴇssɪᴏɴ", callback_data="generate")]
+    ]
+
+    # Rest Buttons
+    buttons = [
+        [InlineKeyboardButton("sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ sᴇssɪᴏɴ", callback_data="generate")],
+        [InlineKeyboardButton("ᴍᴀɪɴᴛᴀɴᴇᴅ ʙʏ​", url="https://t.me/triplenineee")],
+        [
+            InlineKeyboardButton("ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍᴇ​​", callback_data="help"),
+            InlineKeyboardButton("ᴀʙᴏᴜᴛ​", callback_data="about")
+        ],
+        [InlineKeyboardButton("ɪɴꜰo ʟᴀɪɴɴʏᴀ​", url="https://t.me/narumisupport")],
+    ]
+
+    # Help Message
+    HELP = """
+✨ **Available Commands** ✨
+
+/about - Tentang Bot ini
+/help - This Message
+/start - Mulai Bot
+/generate - Mulai Generating Session
+/cancel - Membatalkan process
+/restart - Membatalkan process
+"""
+
+    # About Message
+    ABOUT = """
+**About This Bot** 
+
+Sebuah telegram bot untuk mengambil pyrogram dan telethon string session by @ZhuXstringBot
+
+Group Support : [Gabung](https://t.me/narumisupport)
+
+Framework : [Pyrogram](docs.pyrogram.org)
+
+Language : [Python](www.python.org)
+
+Developer : @triplenineee
+    """
